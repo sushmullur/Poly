c577658b89db1814fb10752263e34c2c28562a58
sushpicious <2011658@apps.nsd.org>
2022-04-06 10:07:14 -0700

    Test file modified.

---
+++ main.cpp
@@ -0,0 +1,42 @@
+// Sush Mullur
+// Assignment 1
+// Test file
+#include <iostream>
+#include "Poly.h"
+
+void test1() {
+    //Test constructors.
+    Poly p;
+    Poly p1(5);
+    Poly p2(4, 5);
+    Poly p3(p2);
+    cout << p3 << endl;
+
+    //Test + operator
+    cout << Poly((p2) + Poly(2, 5) + Poly(3, 4) + Poly(1, 0)) << endl;
+    cout << Poly((p3 + p3)) << endl;
+
+    //Test - operator.
+    cout << (p1 - p2) << endl;
+    cout << Poly((p2) + Poly(2, 5) - Poly(3, 4) + Poly(1, 0)) << endl;
+    cout << (p2 - p2) << endl;
+
+    //Test = operator.
+    Poly p4(4, 4);
+    cout << "Before assignment: " << p4 << endl;
+    p4 = p2;
+    cout << "After assignment: " << p4 << endl;
+    //Test == operator.
+
+    //Test * operator.
+
+}
+
+
+
+
+int main() {
+    test1();
+    cout << "All tests pass." << endl;
+    return 0;
+}
